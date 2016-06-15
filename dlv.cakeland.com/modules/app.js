ddf76b57a9d45c1ed97243ffd1e0b58e4a42c4e8
sunyun <yunsun@cakeland.com>
2016-06-15 17:42:35 +0800

    增加必要包

---
+++ dlv.cakeland.com/modules/app.js
@@ -0,0 +1,5 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    console.log("ok");
+});
+//# sourceMappingURL=app.js.map
---
+++ dlv.cakeland.com/modules/app.tsx
@@ -0,0 +1,5 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+import * as React from "react";
+console.log("ok");
